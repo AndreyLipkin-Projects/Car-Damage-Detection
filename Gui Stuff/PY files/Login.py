f87b7256547499855906eb068331ca070baec14e
AndreyProject <39874401+AndreyProject@users.noreply.github.com>
2019-12-23 23:36:06 +0200

    Add files via upload
    Updated gui files (forward function works)

--- Gui Stuff/PY files/Login.py
+++ Gui Stuff/PY files/Login.py
@@ -8,53 +8,58 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+import sys
+import MainMenu
 
+user_list = ["andrey"]
+pass_list = ["123"]
 
 class Ui_LoginWindow(object):
     def setupUi(self, LoginWindow):
         LoginWindow.setObjectName("LoginWindow")
-        LoginWindow.resize(299, 344)
+        LoginWindow.resize(317, 382)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/Logos/output-onlinepngtools (2).png"), QtGui.QIcon.Selected, QtGui.QIcon.On)
+        icon.addPixmap(QtGui.QPixmap('Logo.png'), QtGui.QIcon.Selected, QtGui.QIcon.On)
         LoginWindow.setWindowIcon(icon)
         LoginWindow.setDocumentMode(False)
         self.centralwidget = QtWidgets.QWidget(LoginWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(70, 210, 41, 16))
+        self.label.setGeometry(QtCore.QRect(50, 220, 61, 31))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(70, 240, 41, 16))
+        self.label_2.setGeometry(QtCore.QRect(50, 260, 51, 21))
         self.label_2.setObjectName("label_2")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(110, 210, 104, 21))
-        self.textEdit.setObjectName("textEdit")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_2.setGeometry(QtCore.QRect(110, 240, 104, 21))
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(130, 280, 56, 17))
-        self.pushButton.setObjectName("pushButton")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(40, 20, 231, 201))
+        self.UserNameInput = QtWidgets.QTextEdit(self.centralwidget)
+        self.UserNameInput.setGeometry(QtCore.QRect(110, 220, 121, 31))
+        self.UserNameInput.setObjectName("UserNameInput")
+        self.PasswordInput = QtWidgets.QTextEdit(self.centralwidget)
+        self.PasswordInput.setGeometry(QtCore.QRect(110, 260, 121, 31))
+        self.PasswordInput.setObjectName("PasswordInput")
+        self.LoginButton = QtWidgets.QPushButton(self.centralwidget)
+        self.LoginButton.setGeometry(QtCore.QRect(130, 310, 61, 21))
+        self.LoginButton.setObjectName("LoginButton")
+        self.LogoLabel = QtWidgets.QLabel(self.centralwidget)
+        self.LogoLabel.setGeometry(QtCore.QRect(50, 20, 231, 201))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-        self.label_3.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(20, 20, 291, 41))
+        sizePolicy.setHeightForWidth(self.LogoLabel.sizePolicy().hasHeightForWidth())
+        self.LogoLabel.setSizePolicy(sizePolicy)
+        self.LogoLabel.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.LogoLabel.setObjectName("LogoLabel")
+        self.Logotext = QtWidgets.QLabel(self.centralwidget)
+        self.Logotext.setGeometry(QtCore.QRect(20, 20, 301, 41))
         font = QtGui.QFont()
         font.setFamily("Brush Script MT")
         font.setPointSize(28)
         font.setItalic(True)
-        self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
+        self.Logotext.setFont(font)
+        self.Logotext.setObjectName("Logotext")
         LoginWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(LoginWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 299, 18))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 317, 21))
         self.menubar.setObjectName("menubar")
         LoginWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(LoginWindow)
@@ -64,19 +69,57 @@ class Ui_LoginWindow(object):
         self.retranslateUi(LoginWindow)
         QtCore.QMetaObject.connectSlotsByName(LoginWindow)
 
+        self.LoginButton.clicked.connect(self.openMainMenu)
+
+       # self.LoginButton.clicked.connect(self.openMainMenu)
+
     def retranslateUi(self, LoginWindow):
         _translate = QtCore.QCoreApplication.translate
         LoginWindow.setWindowTitle(_translate("LoginWindow", "Login"))
         self.label.setText(_translate("LoginWindow", "UseName:"))
         self.label_2.setText(_translate("LoginWindow", "Password:"))
-        self.pushButton.setText(_translate("LoginWindow", "Login"))
-        self.label_3.setText(_translate("LoginWindow", "<html><head/><body><p><img src=\":/Logos/output-onlinepngtools (2).png\"/></p></body></html>"))
-        self.label_4.setText(_translate("LoginWindow", "<html><head/><body><p><span style=\" color:#00009e;\">Anti Fraud Damage Detector</span></p></body></html>"))
+        self.LoginButton.setText(_translate("LoginWindow", "Login"))
+        self.LogoLabel.setText(_translate("LoginWindow", "<html><head/><body><p><img src=\"Logo.png\"/></p></body></html>"))
+        self.Logotext.setText(_translate("LoginWindow", "<html><head/><body><p><span style=\" font-size:22pt; color:#00009e;\">Anti Fraud Damage Detector</span></p></body></html>"))
+
+
+    def openMainMenu(self):
+        username = self.UserNameInput.toPlainText()
+        password = self.PasswordInput.toPlainText()
+        try :
+            if user_list.index(username) != ValueError:
+                if password == pass_list[user_list.index(username)]:
+                    self.mainmenuwindow()
+                else:
+                    raise Exception(self.display_error_message("Username / password incorrect"))
+        except ValueError:
+            self.display_error_message("no such user")
+
+
+        #self.display_error_message("the username is:" + username + "the password is:" + password)
+
+
+    def mainmenuwindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = MainMenu.Ui_MainMenuWindow()
+        self.ui.setupUi(self.window)
+        #MainApplicationWindow.hide()
+        self.window.show()
+
+
+    def display_error_message(self, message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText(message)
+        msg.setWindowTitle("Error")
+        msg.exec_()
+
+
+
 
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainApplicationWindow = QtWidgets.QMainWindow()
     ui = Ui_LoginWindow()
--- Gui Stuff/PY files/MainMenu.py
+++ Gui Stuff/PY files/MainMenu.py
@@ -8,75 +8,104 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap
+
+import Hyperparameters
+import Login
+import Settings
+import Upload_image
 
 
-class Ui_LoginWindow(object):
-    def setupUi(self, LoginWindow):
-        LoginWindow.setObjectName("LoginWindow")
-        LoginWindow.resize(299, 371)
+class Ui_MainMenuWindow(object):
+
+    def setupUi(self, MainMenuWindow):
+        MainMenuWindow.setObjectName("MainMenuWindow")
+        MainMenuWindow.resize(332, 375)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/Logos/output-onlinepngtools (2).png"), QtGui.QIcon.Selected, QtGui.QIcon.On)
-        LoginWindow.setWindowIcon(icon)
-        LoginWindow.setDocumentMode(False)
-        self.centralwidget = QtWidgets.QWidget(LoginWindow)
+        icon.addPixmap(QtGui.QPixmap('Logo.png'), QtGui.QIcon.Selected, QtGui.QIcon.On)
+        MainMenuWindow.setWindowIcon(icon)
+        MainMenuWindow.setDocumentMode(False)
+        self.centralwidget = QtWidgets.QWidget(MainMenuWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(40, 20, 231, 201))
+        self.Logo = QtWidgets.QLabel(self.centralwidget)
+        self.Logo.setPixmap(QPixmap('Logo.png'))
+        self.Logo.setGeometry(QtCore.QRect(50, 20, 231, 201))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-        self.label_3.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(20, 20, 291, 41))
+        sizePolicy.setHeightForWidth(self.Logo.sizePolicy().hasHeightForWidth())
+        self.Logo.setSizePolicy(sizePolicy)
+        self.Logo.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.Logo.setObjectName("Logo")
+
+        self.LogoName = QtWidgets.QLabel(self.centralwidget)
+        self.LogoName.setGeometry(QtCore.QRect(10, 20, 311, 41))
         font = QtGui.QFont()
         font.setFamily("Brush Script MT")
         font.setPointSize(28)
         font.setItalic(True)
-        self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(110, 240, 91, 17))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(110, 270, 91, 16))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(130, 310, 56, 17))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(0, 310, 56, 17))
-        self.pushButton_4.setObjectName("pushButton_4")
-        LoginWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(LoginWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 299, 18))
+        self.LogoName.setFont(font)
+        self.LogoName.setObjectName("LogoName")
+        self.LocalizationButton = QtWidgets.QPushButton(self.centralwidget)
+        self.LocalizationButton.setGeometry(QtCore.QRect(110, 230, 101, 21))
+        self.LocalizationButton.setObjectName("LocalizationButton")
+        self.TrainButton = QtWidgets.QPushButton(self.centralwidget)
+        self.TrainButton.setGeometry(QtCore.QRect(110, 260, 101, 21))
+        self.TrainButton.setObjectName("TrainButton")
+        self.BackButton = QtWidgets.QPushButton(self.centralwidget)
+        self.BackButton.setGeometry(QtCore.QRect(130, 310, 51, 21))
+        self.BackButton.setObjectName("BackButton")
+        MainMenuWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainMenuWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 332, 21))
         self.menubar.setObjectName("menubar")
-        LoginWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(LoginWindow)
+        MainMenuWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainMenuWindow)
         self.statusbar.setObjectName("statusbar")
-        LoginWindow.setStatusBar(self.statusbar)
+        MainMenuWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(LoginWindow)
-        QtCore.QMetaObject.connectSlotsByName(LoginWindow)
+        self.TrainButton.clicked.connect(self.Settingswindow)
+        self.retranslateUi(MainMenuWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainMenuWindow)
 
-    def retranslateUi(self, LoginWindow):
+        self.BackButton.clicked.connect(self.backfunc)
+        self.TrainButton.clicked.connect(self.Settingswindow)
+        self.LocalizationButton.clicked.connect(self.Classificationswindow)
+
+    def retranslateUi(self, MainMenuWindow):
         _translate = QtCore.QCoreApplication.translate
-        LoginWindow.setWindowTitle(_translate("LoginWindow", "MainMenu"))
-        self.label_3.setText(_translate("LoginWindow", "<html><head/><body><p><img src=\":/Logos/output-onlinepngtools (2).png\"/></p></body></html>"))
-        self.label_4.setText(_translate("LoginWindow", "<html><head/><body><p><span style=\" color:#00009e;\">Anti Fraud Damage Detector</span></p></body></html>"))
-        self.pushButton.setText(_translate("LoginWindow", "Get damage localization"))
-        self.pushButton_2.setText(_translate("LoginWindow", "View images"))
-        self.pushButton_3.setText(_translate("LoginWindow", "Back"))
-        self.pushButton_4.setText(_translate("LoginWindow", "Settings"))
+        MainMenuWindow.setWindowTitle(_translate("MainMenuWindow", "MainMenu"))
+        self.Logo.setText(_translate("MainMenuWindow", "<html><head/><body><p><img src=\"Logo.png\"/></p></body></html>"))
+        self.LogoName.setText(_translate("MainMenuWindow", "<html><head/><body><p><span style=\" font-size:24pt; color:#00009e;\">Anti Fraud Damage Detector</span></p></body></html>"))
+        self.LocalizationButton.setText(_translate("MainMenuWindow", "Get Classification"))
+        self.TrainButton.setText(_translate("MainMenuWindow", "Train System"))
+        self.BackButton.setText(_translate("MainMenuWindow", "Back"))
 
+    def backfunc(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Login.Ui_LoginWindow()
+        self.ui.setupUi(self.window)
+        #self.MainApplicationWindow1.hide()
+        self.window.show()
+
+    def Settingswindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Settings.Ui_SettingsWindow()
+        self.ui.setupUi(self.window)
+        #self.hide()
+        self.window.show()
+
+    def Classificationswindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Upload_image.Ui_Classificationswindow()
+        self.ui.setupUi(self.window)
+        #self.hide()
+        self.window.show()
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
-    MainApplicationWindow = QtWidgets.QMainWindow()
-    ui = Ui_LoginWindow()
-    ui.setupUi(MainApplicationWindow)
-    MainApplicationWindow.show()
+    MainApplicationWindow1 = QtWidgets.QMainWindow()
+    ui = Ui_MainMenuWindow()
+    ui.setupUi(MainApplicationWindow1)
+    MainApplicationWindow1.show()
     sys.exit(app.exec_())
