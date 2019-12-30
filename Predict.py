8d9f4c191aab8176e767313b840d84b3e30a0850
lepilmen <lepilmen@gmail.com>
2019-12-30 19:22:38 +0200

    Added prediction functionality
    Function to predict based on an image and a model sent from the UI.

---
+++ Predict.py
@@ -0,0 +1,46 @@
+def Prediction (modelType,img_path) :
+    from keras import Model
+    from keras.engine.saving import model_from_json
+    from keras.layers import GlobalAveragePooling2D, Dense
+    from keras.models import load_model
+    from keras.preprocessing import image
+    import numpy as np
+    from keras.applications.resnet50 import preprocess_input
+    from keras.applications.resnet50 import decode_predictions
+    img_width, img_height = 300, 300
+
+    '''
+    def load_image(img_path, show=False):
+        img = image.load_img(img_path, target_size=(300,300))
+        img_tensor = image.img_to_array(img)                    # (height, width, channels)
+        img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+        img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+        return img_tensor
+    '''
+
+    # load the model we saved, also, according to the results from the Two_Class model , if the result passes the
+    #the threshold defined for it,
+    modelPath = "./checkpoints/" + modelType + "ResNet50_model.h5"
+    model = load_model(modelPath)
+    img = image.load_img(img_path, target_size=(img_width, img_height))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    print(preds)
+
+    '''
+    ##restore and load
+    json_file = open("./model.json", "r")
+    loaded_model_json = json_file.read()
+    json_file.close()
+    
+    model = model_from_json(loaded_model_json)
+    
+    model.load_weights("./weights.h5")
+    
+    model.compile(loss='mean_squared_error', optimizer="RMSprop")
+    predicts = model.predict(x)
+    #print(predicts[0])
+    '''
+    return preds
